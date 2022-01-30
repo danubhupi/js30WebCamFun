1d9b409a2700e5d9bc7f8d242612e35ef34532bb
Bhupendra Singh <danubhupi@gmail.com>
2022-01-30 14:35:29 +0530

    Added buttons for diff modes

--- scripts.js
+++ scripts.js
@@ -1,96 +1,95 @@
-const video = document.querySelector('.player');
-const canvas = document.querySelector('.photo');
-const ctx = canvas.getContext('2d');
-const strip = document.querySelector('.strip');
-const snap = document.querySelector('.snap');
+const video = document.querySelector(".player");
+const canvas = document.querySelector(".photo");
+const ctx = canvas.getContext("2d");
+const strip = document.querySelector(".strip");
+const snap = document.querySelector(".snap");
+let mode = "none";
 
+function getsVideo() {
+  // console.log(video.attributes);
+  navigator.mediaDevices
+    .getUserMedia({ audio: false, video: true })
+    .then((mediastream) => {
+      video.srcObject = mediastream;
+      video.play();
+    })
+    .catch((error) => console.log(error));
 
- function getsVideo(){ 
-    // console.log(video.attributes);
-     navigator.mediaDevices.getUserMedia({audio:false,video:true})
-     .then(mediastream =>{
-        video.srcObject= mediastream;
-        video.play();
+  // video.src = window.URL.createObjectURL(mediastream);
 
-     }).catch(error=> console.log(error));
-
-    
-    
-    // video.src = window.URL.createObjectURL(mediastream);
-    
-    // console.log(video.clientWidth);
+  // console.log(video.clientWidth);
 }
 
-pasteToCanvas=()=>{
-    
-     const width=video.videoWidth;
-     const height=video.videoHeight;
-    // console.log(pixels);
-    //  getSnap();
+setMode = (modeReq) => {
+  mode = modeReq;
+};
 
-     return setInterval(()=>{
-         canvas.width=width;
-         canvas.height=height;
-    ctx.drawImage(video,0,0,width,height);
+pasteToCanvas = () => {
+  const width = video.videoWidth;
+  const height = video.videoHeight;
+  // console.log(pixels);
+  //  getSnap();
 
-    let pixels=ctx.getImageData(0,0,width,height);
+  return setInterval(() => {
+    canvas.width = width;
+    canvas.height = height;
+    ctx.drawImage(video, 0, 0, width, height);
 
+    let pixels = ctx.getImageData(0, 0, width, height);
 
     // pixels=redShift(pixels);
-    pixels=rgbSplit(pixels);
-    ctx.putImageData(pixels,0,0);
-    
-     },20);
-   
-    
-}
+    if (mode === "redshift") {
+      pixels = redShift(pixels);
+      ctx.putImageData(pixels, 0, 0);
+    }
 
-getSnap=()=>{
-    snap.currentTime=0;
-    snap.play();
-    // const width=video.videoWidth;
-    //  const height=video.videoHeight;
-    // console.log(ctx.getImageData(0,0,width,height));
-    let link=document.createElement('a');
-    link.setAttribute('download','DanuDoes');
-   
-    let img=document.createElement('img');
-    src=canvas.toDataURL('image/jpeg');
-    link.setAttribute('href',`${src}`);
-    img.src=src;
-    link.appendChild(img);
-    strip.appendChild(link);
-}
+    if (mode === "rgbshift") {
+      pixels = rgbSplit(pixels);
+      ctx.putImageData(pixels, 0, 0);
+    }
+  }, 20);
+};
 
+getSnap = () => {
+  snap.currentTime = 0;
+  snap.play();
+  // const width=video.videoWidth;
+  //  const height=video.videoHeight;
+  // console.log(ctx.getImageData(0,0,width,height));
+  let link = document.createElement("a");
+  link.setAttribute("download", "DanuDoes");
 
-redShift=(pixels)=>{
-   
-    for(let i=0;i<pixels.data.length;i+=4){
-        pixels.data[i + 0]=pixels.data[i + 0] +255 ;
-        pixels.data[i + 1]=pixels.data[i + 1]+0;
-        pixels.data[i + 2]=pixels.data[i + 2]+0;
-        // pixels.data[i + 3]=pixels.data[i + 3]+0.1;
-        }
-    return pixels;
+  let img = document.createElement("img");
+  src = canvas.toDataURL("image/jpeg");
+  link.setAttribute("href", `${src}`);
+  img.src = src;
+  link.appendChild(img);
+  strip.appendChild(link);
+};
 
-}
+redShift = (pixels) => {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i + 0] = pixels.data[i + 0] + 255;
+    pixels.data[i + 1] = pixels.data[i + 1] + 0;
+    pixels.data[i + 2] = pixels.data[i + 2] + 0;
+    // pixels.data[i + 3]=pixels.data[i + 3]+0.1;
+  }
+  return pixels;
+};
 
+rgbSplit = (pixels) => {
+  for (let i = 0; i < pixels.data.length; i += 4) {
+    pixels.data[i - 100] = pixels.data[i + 0];
+    pixels.data[i - 150] = pixels.data[i + 1];
+    pixels.data[i + 200] = pixels.data[i + 2];
+    // pixels.data[i + 3]=pixels.data[i + 3]+0.1;
+  }
 
-
-rgbSplit=(pixels)=>{
-    for(let i=0;i<pixels.data.length;i+=4){
-        pixels.data[i -100]=pixels.data[i +0]  ;
-        pixels.data[i - 150]=pixels.data[i+1 ];
-        pixels.data[i + 200]=pixels.data[i+2 ];
-        // pixels.data[i + 3]=pixels.data[i + 3]+0.1;
-        }
-
-return pixels;
-}
-
+  return pixels;
+};
 
 // setInterval(getsVideo(),1000);
 getsVideo();
 // setInterval(pasteToCanvas,20);
-video.addEventListener('canplay',pasteToCanvas);
+video.addEventListener("canplay", pasteToCanvas);
 // pasteToCanvas();
